d0fd5e1f9a47801495eb869221ad295f599be534
Kalyan <kalyan.07x@gmail.com>
2026-09-13 07:29:42 +0300

    Auto-sync de-prep 2026-09-13 07:29:42

--- de-prep/java/basics.java
+++ de-prep/java/basics.java
@@ -1,7 +1,12 @@
 public class basics {
     
-    public void print(<T> t){
-
+    public static <T> void print(T t){
+        System.out.println(t);
     }
+
+    public static void main (String... args){
+        print("We practice all the Java basics here.");
+    }
+        
 
 }
