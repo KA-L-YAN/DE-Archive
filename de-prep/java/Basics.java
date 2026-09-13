b92f761e870ffba44d67780a3e35ed0f2f444b21
Kalyan <kalyan.07x@gmail.com>
2026-09-13 09:22:42 +0300

    Auto-sync de-prep 2026-09-13 09:22:42

--- de-prep/java/Basics.java
+++ de-prep/java/Basics.java
@@ -34,5 +34,10 @@ public class Basics {
 /**
  * 1. I have tried it out and it print false. the equal seems correct but here why I think it print false,
  * We have added 1,2 to the set with add() with one anonymous object and the we created another anonymous object to check if 1 and 2 exist or not.
- * 2. 
+ * 2. No, can you please explain this. I have always had doubts around this concept...
+ * 3. Animal and woof! ( when we create an object of type Animal referencing to the Dog class, the compiler checks for super class methods implementation in the change class and thats why woof was printed since it has overridden speak() method of parent class)
+ * This has nothing to do with the name variable
+ * 4. Base init
+ * 5. no.
+ * 6. Yes. but super() should be in the first line of the constructor
  */
