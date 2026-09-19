13acc636fb0fc91578990d42b8015d47e90a5e29
Kalyan <kalyan.07x@gmail.com>
2026-09-19 10:32:45 +0300

    Auto-sync de-prep 2026-09-19 10:32:45

--- de-prep/java/practice/Book.java
+++ de-prep/java/practice/Book.java
@@ -3,17 +3,41 @@ package practice;
 public class Book {
     private String title;
     private String author;
-    private String isbn;
+    private long isbn;
     private boolean isAvailable;
 
     public Book(){
 
     }
 
-    public Book(String title, String author, String isbn, boolean isAvailable)
+    public Book(String title, String author, long isbn, boolean isAvailable)
     {
         this.title = title;
         this.author = author;
-        this.isbn = 
+        this.isbn = isbn;
+        this.isAvailable = isAvailable;
     }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public String getTitle(){
+        return this.title;
+    }
+    public void setAuthor(String author){
+        this.author = author;
+    }
+    public String getAuthor(){
+        return this.author;
+    }
+    public void setIsbn(long isbn){
+        this.isbn = isbn;
+    }
+    public long getIsbn(){
+        return this.isbn;
+    }
+    public void setAvailability(boolean isAvailable){
+        this.isAvailable = isAvailable;
+    }
+    public 
 }
